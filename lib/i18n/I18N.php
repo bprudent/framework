e416c4cedac3b48d762d487a80c8a8dccf2343c6
Joshua T Corbin <jcorbin@wunjo.org>
2009-03-15 13:07:15 -0400

    Update license blocks
    Update license blocks to be proper MPL/GPL/LGPL tri-licenses with contributor
    attribution pulled from git history.

--- lib/i18n/I18N.php
+++ lib/i18n/I18N.php
@@ -1,28 +1,40 @@
 <?php
-
-/**
- * I18N class definition
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * PHP version 5
- *
- * LICENSE: The contents of this file are subject to the Mozilla Public License Version 1.1
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- * The Original Code is Red Tree Systems Code.
+ * The Original Code is RedTree Framework I18N Module
  *
- * The Initial Developer of the Original Code is Red Tree Systems, LLC. All Rights Reserved.
+ * The Initial Developer of the Original Code is RedTree Systems LLC.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
  *
- * @category     i18n
- * @author       Red Tree Systems, LLC <support@redtreesystems.com>
- * @copyright    2007 Red Tree Systems, LLC
- * @license      MPL 1.1
- * @version      1.0
- * @link         http://framework.redtreesystems.com
+ * Contributor(s):
+ *   Brandon Prudent <bprudent@redtreesystems.com>
+ *   Joshua T Corbin <jcorbin@redtreesystems.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the LGPL or the GPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK *****
  */
 
 /**
